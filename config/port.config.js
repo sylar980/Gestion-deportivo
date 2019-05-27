5077eb74f701b090da1af35044e903d091af4f51
Jon Oliva <jonoliva18@gmail.com>
2019-05-26 20:48:36 -0400

    Co-authored-by: Andy Macias <xandyx2014@gmail.com>

--- config/port.config.js
+++ config/port.config.js
@@ -3,5 +3,4 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-
 module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require('./app');
 const config = require('./config/port.config');
+const moongose = require('mongoose');
 
-app.listen(config.get('port') || 3000, () => {
-  console.log(`Conexion Correcta escuchando en el puerto 3000`);
+moongose.connect('mongodb://localhost/bdsis', {
+  useNewUrlParser: true
+}).then( () => {
+    app.listen(config.get('port'), () => {
+      console.log('conectado a la base de datos');
+      console.log(`Conexion Correcta escuchando en el puerto 3000`);
+    });
 });
